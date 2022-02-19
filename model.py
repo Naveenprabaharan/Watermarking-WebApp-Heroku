e634a1174c30f5ba008b247b562f952d81f68266
Naveenprabaharan <83350292+Naveenprabaharan@users.noreply.github.com>
2022-02-19 12:26:27 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import cv2
 from PIL import Image
 import numpy as np
-import time
+import base64
+import io
+
 
 def Water_Mark(Water_Mark_image, Water_Mark_Text, Water_Mark_Position, Water_Mark_Text_size):
     path = Water_Mark_image
@@ -28,12 +30,12 @@ def Water_Mark(Water_Mark_image, Water_Mark_Text, Water_Mark_Position, Water_Mar
     elif (Water_Mark_Position == 'Bottom Right'):
         x = int((watermarksize[0] * 0.60))
         y = int((watermarksize[1] * 0.9))
-    elif(Water_Mark_Position == 'Bottom Left'):
+    elif (Water_Mark_Position == 'Bottom Left'):
         x = int(watermarksize[0] - (watermarksize[0] * 0.90))
         y = int((watermarksize[1] * 0.9))
     else:
-        x = int(watermarksize[0]/2)
-        y = int((watermarksize[1] /2))
+        x = int(watermarksize[0] / 2)
+        y = int((watermarksize[1] / 2))
     org = (x, y)
 
     # fontScale
@@ -47,9 +49,10 @@ def Water_Mark(Water_Mark_image, Water_Mark_Text, Water_Mark_Position, Water_Mar
 
     # Using cv2.putText() method
     image = cv2.putText(frame, Water_Mark_Text, org, font, fontScale, color, thickness, cv2.LINE_AA)
+    image = Image.fromarray(image)
+    data = io.BytesIO()
 
-    t_picname = time.time() #storing name as time in sec
-    filename = f'static/final/{t_picname}.jpg'
-    cv2.imwrite(filename, image) #image storing
+    image.save(data, format="png")
+    encoded_img_data = base64.b64encode(data.getvalue())
 
-    return t_picname
+    return encoded_img_data
